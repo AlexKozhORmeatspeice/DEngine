f0154a6794a164407f0f6e01b84e4ddc4732033d
meatspeice <54778699+AlexKozhORmeatspeice@users.noreply.github.com>
2026-06-23 14:45:26 +0500

    temp

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Lenovo/Desktop/Programing/GameDev/B++/DEngine
-# Build directory: C:/Users/Lenovo/Desktop/Programing/GameDev/B++/DEngine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
