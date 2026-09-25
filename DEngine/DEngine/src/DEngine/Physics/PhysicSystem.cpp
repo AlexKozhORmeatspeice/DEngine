68f9c94595147b6b453c68f342dc57c2580b0afd
Konstantin Semischastnov <k7happy2@gmail.com>
2026-09-26 01:11:13 +0300

    added basic phisycs
    Applies gravity, bounces of y=-40

--- DEngine/DEngine/src/DEngine/Physics/PhysicSystem.cpp
+++ DEngine/DEngine/src/DEngine/Physics/PhysicSystem.cpp
@@ -7,14 +7,38 @@ namespace DEngine
 {
 	void PhysicsSystem::OnUpdate(const Timestep& ts, const Scene* scene)
 	{
-		auto physicsComponents = scene->View<ColliderComponent, RigidbodyComponent>();
+		auto physicsComponents = scene->View<ColliderComponent, RigidbodyComponent, TransformComponent>();
 
 		//TODO: Здесь короч, Кость, тебе писать нужно основной код. Воооооооот. Можешь еще всякие преколы делать
 		//Для ориентира можешь смотреть как написано класс MeshRendererSystem
 
-		for (auto [entity, collider, rigidbody] : physicsComponents.each())
-		{
+		for (auto [entity, collider, rigidbody, transform] : physicsComponents.each())
+		{	
 			//Здесь короч чист обработка всех сущностей разом со всеми компонентами
+
+			if (rigidbody.isKinematic)
+			{
+				continue;
+			}
+
+			if (rigidbody.useGravity)
+			{
+				rigidbody.force = glm::vec3(0, -9.81 * rigidbody.mass, 0);
+			}
+
+			float dt = ts.GetSeconds();
+			rigidbody.acceleration += rigidbody.force / rigidbody.mass;
+			rigidbody.velocity += rigidbody.acceleration * dt;
+			glm::vec3 dPos = rigidbody.velocity * dt;
+			transform.SetPosition(transform.GetPosition() + dPos);
+
+			glm::vec3 curPos = transform.GetPosition();
+			if (curPos[1] <= -40.0)
+			{
+				curPos[1] = -40.0;
+				transform.SetPosition(curPos);
+				rigidbody.velocity = -rigidbody.velocity * 0.85f;
+			}
 		}
 	}
 }
